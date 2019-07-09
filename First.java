42ddddae03b18942af55b62c31c283cc6f805a4e
desertsnumbertheory <52709047+madihahouri@users.noreply.github.com>
2019-07-09 17:00:43 +0200

    Add files via upload

---
+++ First.java
@@ -0,0 +1,13 @@
+
+package first;
+// this is my first program
+// my name is Number Theory Deserts
+public class First {
+
+    public static void main(String[] args) {
+       System.out.print("Welcome");
+       System.out.println(" Java programming");
+       
+    }
+    
+}
