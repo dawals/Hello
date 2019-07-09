2b9616c277d1f8c5d654bccb8f4504523a32bbd1
desertsnumbertheory <52709047+madihahouri@users.noreply.github.com>
2019-07-09 17:03:29 +0200

    Add files via upload

---
+++ If_statement1.java
@@ -0,0 +1,25 @@
+
+package if_statement1;
+import java.util.Scanner;
+
+public class If_statement1 {
+
+    public static void main(String[] args) {
+     Scanner in = new Scanner( System.in);
+     
+     
+      int p=50;
+      
+      if (p%2==0)
+      {
+          System.out.print("Even");
+          if(p%5==1)System.out.print("odd");
+      }
+      
+      
+      
+      
+
+    }
+    
+}
