8d4ac381fe6a868c636d4e25f65e4c97cc00abcb
desertsnumbertheory <52709047+madihahouri@users.noreply.github.com>
2019-07-09 16:59:45 +0200

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,23 @@
+
+package add;
+
+
+public class Add {
+
+    
+    public static void main(String[] args) {
+       int number1;
+       int number2;
+       int sum;
+       
+       number1 = 5
+       number2 = 0;
+       
+       sum = number1 + number2;
+       System.out.print("Sum is " + sum);
+       
+       
+               
+    }
+    
+}
