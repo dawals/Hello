b0a11899a4ab569efe66704123d12bd71cd16308
desertsnumbertheory <52709047+madihahouri@users.noreply.github.com>
2019-07-09 16:55:03 +0200

    addition.3
    java

---
+++ Addition3.java
@@ -0,0 +1,15 @@
+
+package addition3;
+
+public class Addition3 {
+
+    public static void main(String[] args) {
+     int g1, g2, g3;
+        double av;
+        g1=70; g2=80; g3=85;
+        av = (g1 + g2 + g3)/3.0;
+        System.out.printf("%c+%c",'A','B');
+           
+    }
+    
+}
