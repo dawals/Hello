b34699193b5209e8d01364f3721fc63ed1a80aa4
desertsnumbertheory <52709047+madihahouri@users.noreply.github.com>
2019-07-09 17:02:16 +0200

    Add files via upload

---
+++ Scanner1.java
@@ -0,0 +1,30 @@
+
+package scanner1;
+
+
+public class Scanner1 {
+
+    public static void main(String[] args) {
+        double x=2, y=3, z=4, d=43, e=0;
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.sqrt(z));
+        System.out.println(Math.max(y, x));
+        System.out.println(Math.ceil(3.4));
+        System.out.println(Math.floor(7.8));
+        System.out.println(Math.round(3.4));
+        x++;
+        System.out.println(x);
+        z--;
+        System.out.println(z);
+        System.out.println(z*=2);
+        System.out.println(x<0);
+        System.out.println(x>=2 && --z>=6);
+        e = d/10 + d%10;
+        System.out.println(e);
+        
+               
+ 
+        
+    }
+    
+}
